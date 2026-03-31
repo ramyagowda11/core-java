c6592afd2eb63f1a928ff4405d916353de3a4c09
ramyagowda11 <ramyagowdabk79@gmail.com>
2026-03-31 00:30:52 -0700

    task 30-03-2026

---
+++ 30-3-2026/InstagramRunner.java
@@ -0,0 +1,9 @@
+class InstagramRunner{
+public static void main(String[] args){
+	Instagram media=new Instagram();
+	media .login();
+	media .login("Face book ID");
+	media .login("ramya","ramya@123");
+	media .login(96869293183l,"ramya2456");
+}
+}
