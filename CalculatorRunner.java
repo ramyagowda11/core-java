fbe9a5fcb2917c5a260a18bd076d84ed4941d9a2
ramyagowda11 <ramyagowdabk79@gmail.com>
2026-03-30 22:29:53 -0700

    task 30-3-2026

---
+++ CalculatorRunner.java
@@ -0,0 +1,113 @@
+class CalculatorRunner{
+	public static void main(String[] calculator){
+		System.out.println("addition of parameter number");
+		Calculator sum=new Calculator();
+		System.out.println("addition of parameter:"+sum.add(1));
+		System.out.println("addition of parameter again:"+sum.add(5));
+		System.out.println("--------");
+		System.out.println("addition of two number 1st time:"+sum.add(1,2));
+		System.out.println("addition of two number 2nd time:"+sum.add(2,3));
+		System.out.println("--------");
+		System.out.println("addition of three number 1st time:"+sum.add(1,2,3));
+		System.out.println("addition of three number 2nd time:"+sum.add(3,4,5));
+		System.out.println("--------");
+		System.out.println("addition of four number 1st time:"+sum.add(1,2,3,4));
+		System.out.println("addition of four number 2nd time:"+sum.add(3,4,5,6));
+		System.out.println("--------");
+		System.out.println("addition of five number 1st time:"+sum.add(1,2,3,4,5));
+		System.out.println("addition of five number 2nd time:"+sum.add(3,4,5,6,7));
+		System.out.println("--------");
+		System.out.println("addition of six number 1st time:"+sum.add(1,2,3,4,5,6));
+		System.out.println("addition of six number 2nd time:"+sum.add(3,4,5,6,7,8));
+		System.out.println("--------");
+		System.out.println("addition of seven number 1st time:"+sum.add(1,2,3,4,5,6,7));
+		System.out.println("addition of seven number 2nd time:"+sum.add(3,4,5,6,7,8,9));
+		System.out.println("--------");
+		System.out.println("addition of eight number 1st time:"+sum.add(1,2,3,4,5,6,7,8));
+		System.out.println("addition of eight number 2nd time:"+sum.add(3,4,5,6,7,8,9,10));
+		System.out.println("--------");
+		System.out.println("addition of nine number 1st time:"+sum.add(1,2,3,4,5,6,7,8,9));
+		System.out.println("addition of nine number 2nd time:"+sum.add(3,4,5,6,7,8,9,10,11));
+		System.out.println("--------");
+		System.out.println("addition of ten number 1st time:"+sum.add(1,2,3,4,5,6,7,8,9,10));
+		System.out.println("addition of three number 2nd time:"+sum.add(3,4,5,6,7,8,9,10,11,12));
+		System.out.println("**************");
+		
+		
+		
+		
+		System.out.println("subtraction of parameter number");
+		Calculator sub=new Calculator();
+		System.out.println("subtraction of number 1st time:"+sub.minus(1));
+		System.out.println("subtraction of number 2nd time:"+sub.minus(3));
+		System.out.println("--------");
+		System.out.println("subtraction of two number 1st time:"+sub.minus(1,2));
+		System.out.println("subtraction of two number 2nd time:"+sub.minus(3,4));
+		System.out.println("--------");
+		System.out.println("subtraction of three number 1st time:"+sub.minus(1,2,3));
+		System.out.println("subtraction of three number 2nd time:"+sub.minus(3,4,5));
+		System.out.println("--------");
+		System.out.println("subtraction of four number 1st time:"+sub.minus(1,2,3,4));
+		System.out.println("subtraction of four number 2nd time:"+sub.minus(3,4,5,6));
+		System.out.println("--------");
+		System.out.println("subtraction of five number 1st time:"+sub.minus(1,2,3,4,5));
+		System.out.println("subtraction of five number 2nd time:"+sub.minus(3,4,5,6,7));
+		System.out.println("--------");
+		System.out.println("subtraction of six number 1st time:"+sub.minus(1,2,3,4,5,6));
+		System.out.println("subtraction of six number 2nd time:"+sub.minus(3,4,5,6,7,8));
+		System.out.println("--------");
+		System.out.println("subtraction of seven number 1st time:"+sub.minus(1,2,3,4,5,6,7));
+		System.out.println("subtraction of seven number 2nd time:"+sub.minus(3,4,5,6,7,8,9));
+		System.out.println("--------");
+		System.out.println("subtraction of eight number 1st time:"+sub.minus(1,2,3,4,5,6,7,8));
+		System.out.println("subtraction of eight number 2nd time:"+sub.minus(3,4,5,6,7,8,9,10));
+		System.out.println("--------");
+		System.out.println("subtraction of nine number 1st time:"+sub.minus(1,2,3,4,5,6,7,8,9));
+		System.out.println("subtraction of nine number 2nd time:"+sub.minus(3,4,5,6,7,8,9,10,11));
+		System.out.println("--------");
+		System.out.println("subtraction of ten number 1st time:"+sub.minus(1,2,3,4,5,6,7,8,9,10));
+		System.out.println("subtraction of ten number 2nd time:"+sub.minus(3,4,5,6,7,8,9,10,11,12));
+		System.out.println("**************");
+		
+		
+		System.out.println("multiplication of parameter number");
+		Calculator product=new Calculator();
+		System.out.println("multiplication of number 1st time:"+product.multiply(1));
+		System.out.println("multiplication of number 2nd time:"+product.multiply(3));
+		System.out.println("--------");
+		System.out.println("multiplication of two number 1st time:"+product.multiply(1,2));
+		System.out.println("multiplication of two number 2nd time:"+product.multiply(3,4));
+		System.out.println("--------");
+		System.out.println("multiplication of three number 1st time:"+product.multiply(1,2,3));
+		System.out.println("multiplication of three number 2nd time:"+product.multiply(3,4,5));
+		System.out.println("--------");
+		System.out.println("multiplication of four number 1st time:"+product.multiply(1,2,3,4));
+		System.out.println("multiplication of four number 2nd time:"+product.multiply(3,4,5,6));
+		System.out.println("--------");
+		System.out.println("multiplication of five number 1st time:"+product.multiply(1,2,3,4,5));
+		System.out.println("multiplication of five number 2nd time:"+product.multiply(3,4,5,6,7));
+		System.out.println("--------");
+		System.out.println("multiplication of six number 1st time:"+product.multiply(1,2,3,4,5,6));
+		System.out.println("multiplication of six number 2nd time:"+product.multiply(3,4,5,6,7,8));
+		System.out.println("--------");
+		System.out.println("multiplication of seven number 1st time:"+product.multiply(1,2,3,4,5,6,7));
+		System.out.println("multiplication of seven number 2nd time:"+product.multiply(3,4,5,6,7,8,9));
+		System.out.println("--------");
+		System.out.println("multiplication of eight number 1st time:"+product.multiply(1,2,3,4,5,6,7,8));
+		System.out.println("multiplication of eight number 2nd time:"+product.multiply(3,4,5,6,7,8,9,10));
+		System.out.println("--------");
+		System.out.println("multiplication of nine number 1st time:"+product.multiply(1,2,3,4,5,6,7,8,9));
+		System.out.println("multiplication of nine number 2nd time:"+product.multiply(3,4,5,6,7,8,9,10,11));
+		System.out.println("--------");
+		System.out.println("multiplication of ten number 1st time:"+product.multiply(1,2,3,4,5,6,7,8,9,10));
+		System.out.println("multiplication of ten number 2nd time:"+product.multiply(3,4,5,6,7,8,9,10,11,12));
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
