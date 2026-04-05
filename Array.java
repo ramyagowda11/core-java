aa29a366bc5d5f0c55ea362941d66b4cab3dabe0
ramyagowda11 <ramyagowdabk79@gmail.com>
2026-04-05 02:27:48 -0700

    Correct array initialization and print statements
    Fix syntax errors in array initialization and loop.

--- Array.java
+++ Array.java
@@ -2,7 +2,7 @@ class Array{
 	public static void main(String []args){
 		String name=array={"anu","spandu","neha"};
 		for(name=index;name<=lengh.index;index++){
-			System.out.println("age:"+age[0]);
+			System.out.println("name:"+name[0]);
 		}
 	}
 }
